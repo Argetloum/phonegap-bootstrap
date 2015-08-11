f12f1f13e9a00ba881e9b7b1d5c4b3f7c011ae54
Argetloum <florian.mithieux@gmail.com>
2015-08-11 15:52:24 +0200

    Add an inAppFilter to manage offline data

--- app/modules/common/js/app_base_service.js
+++ app/modules/common/js/app_base_service.js
@@ -7,9 +7,9 @@
         .module('Services')
         .service('AppBaseService', AppBaseService);
 
-    AppBaseService.$inject = ['BaseService'];
+    AppBaseService.$inject = ['BaseService', 'LocalStorageService'];
 
-    function AppBaseService(BaseService)
+    function AppBaseService(BaseService, LocalStorageService)
     {
         //
         // Private members
@@ -25,22 +25,71 @@
         //
         // Implementation of Base Service extension
         //
+        /**
+         * AppBaseService constructor
+         * @param Api
+         * @param options
+         */
         _service.appBaseService = function(Api, options)
         {
             BaseService.ListKeyService.call(this, Api, options);
         };
-
         _service.appBaseService.prototype = Object.create(BaseService.ListKeyService.prototype);
 
+        /**
+         * Custom implementation of BaseService getList
+         * This implementation permit to set only the listKey in params
+         * @param listKey
+         */
         _service.appBaseService.prototype.displayList = function(listKey)
         {
             return this.getList(undefined, undefined, listKey);
+        };
+
+        /**
+         * Custom implementation of the BaseService filterize method
+         * This implementation permit to handle a fallback when data is not reachable
+         * This fallback looks in the App LocalStorage to find an old version of items and add it to the list.
+         * @param params
+         * @param cb
+         * @param errCb
+         * @param listKey
+         */
+        _service.appBaseService.prototype.inAppFilter = function(params, cb, errCb, listKey)
+        {
+            var _self = this;
+
+            return this.filterize(params, function success(items)
+            {
+                // Put the data list in the localStorage
+                LocalStorageService.setData(listKey, items);
+                cb(items);
+            }, function error(err)
+            {
+                // If no data is founded, check if a list is already defined
+                // If not, get the localStorage data and replace the items list
+                if (LocalStorageService.getKey(listKey) && !_self.displayList(listKey).length)
+                {
+                    var items = LocalStorageService.getData(listKey);
+                    if(angular.isDefined(items) && items.length)
+                    {
+                        _self.displayList(listKey).push.apply(_self.displayList(listKey), items);
+                    }
+                }
+                errCb(err);
+            }, listKey);
         };
 
 
         //
         // PRIVATE METHODS
         //
+        /**
+         * AppBaseService method to create the custom base service
+         * @param Api
+         * @param options
+         * @returns {AppBaseService.appBaseService}
+         */
         function _createAppBaseService(Api, options)
         {
             return new _service.appBaseService(Api, options);
